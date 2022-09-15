5007c9861ad97a19c4a167fcbf0479ec96dd91cd
John Doe <johndoe@example.com>
2022-09-15 12:35:24 +0200

    Console rész

---
+++ src/lista.js
@@ -0,0 +1,24 @@
+let tomb=[];
+
+export function hozzaad(a){
+
+    if(a>=0){
+        tomb.push(a);
+    }else{
+        console.log("Hiba");
+    }
+
+}
+
+export function atlag(){
+    let osszeg=0;
+
+    for (const item of tomb) {
+        osszeg+=item;
+    }
+
+    let atlag=osszeg/tomb.length;
+
+    return atlag;
+}
+
---
+++ src/test.js
@@ -0,0 +1,13 @@
+import { hozzaad } from "./lista.js";
+
+import { atlag } from "./lista.js";
+
+
+hozzaad(5);
+console.log(atlag())
+hozzaad(-5);
+console.log(atlag())
+hozzaad(5);
+console.log(atlag())
+hozzaad(5);
+console.log(atlag())
